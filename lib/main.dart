45fd6714f0483680cb6a981a27cf9ae71be0d732
GhinaNo <ghinwano003@gmail.com>
2024-09-05 02:56:03 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo/pages/todo_list.dart';
+
+void main(){
+  runApp( const ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const TodoListPage(),
+    );
+  }
+}
---
+++ lib/utils/snackbar_helper.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void ShowErrorMessage(
+    BuildContext context,
+    {required String message,
+    }){
+  final snackBar = SnackBar(
+    content: Text(message,
+        style: TextStyle(color: Colors.white),
+    ),
+
+    backgroundColor: Colors.red,);
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+
+void ShowSuccessMessage(BuildContext context,
+    {required String message,
+    }){
+  final snackBar = SnackBar(content: Text(message));
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
